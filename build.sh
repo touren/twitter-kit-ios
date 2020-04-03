cf6b68b008f03a061e3239389906a826bd117a09
Ben Ward <ben@benward.uk>
2020-04-03 00:42:55 -0700

    Do release builds, not debug

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 ## Build TwitterKit.framework - x86_64
 xcodebuild \
     -project TwitterKit/TwitterKit.xcodeproj \
-    -scheme TwitterKit -configuration Debug \
+    -scheme TwitterKit -configuration Release \
     -sdk "iphonesimulator" \
     HEADER_SEARCH_PATHS="$(pwd)/TwitterCore/iphonesimulator/Headers $(pwd)/TwitterCore/iphonesimulator/PrivateHeaders"  \
     CONFIGURATION_BUILD_DIR=./iphonesimulator \
@@ -14,38 +14,33 @@ xcodebuild \
 ## Build TwitterKit.framework - armv7, arm64
 xcodebuild \
     -project TwitterKit/TwitterKit.xcodeproj \
-    -scheme TwitterKit -configuration Debug \
+    -scheme TwitterKit -configuration Release \
     -sdk "iphoneos" \
     HEADER_SEARCH_PATHS="$(pwd)/TwitterCore/iphoneos/Headers $(pwd)/TwitterCore/iphoneos/PrivateHeaders"  \
     CONFIGURATION_BUILD_DIR=./iphoneos \
     clean build
 
 ## Merge into one TwitterKit.framework with x86_64, armv7, arm64
-rm -rf iOS
-mkdir -p iOS
+rm -rf build
+mkdir -p build
 
 # Combine simulator and iphone frameworks
-cp -r TwitterKit/iphoneos/TwitterKit.framework/ iOS/TwitterKit.framework
+cp -r TwitterKit/iphoneos/TwitterKit.framework/ build/TwitterKit.framework
 
-lipo -create -output iOS/TwitterKit.framework/TwitterKit \
+lipo -create -output build/TwitterKit.framework/TwitterKit \
   TwitterKit/iphoneos/TwitterKit.framework/TwitterKit \
   TwitterKit/iphonesimulator/TwitterKit.framework/TwitterKit
 
-lipo -archs iOS/TwitterKit.framework/TwitterKit
+lipo -archs build/TwitterKit.framework/TwitterKit
 
 # Copy bytecode symbol maps (iOS only -- not created for simulator)
-cp -av TwitterKit/iphoneos/*.bcsymbolmap iOS/
+cp -av TwitterKit/iphoneos/*.bcsymbolmap build/
 
 # Combine dSYMs
-cp -r TwitterKit/iphoneos/TwitterKit.framework.dSYM iOS/
+cp -r TwitterKit/iphoneos/TwitterKit.framework.dSYM build/
 
-lipo -create -output iOS/TwitterKit.framework.dSYM/Contents/Resources/DWARF/TwitterKit \
+lipo -create -output build/TwitterKit.framework.dSYM/Contents/Resources/DWARF/TwitterKit \
   TwitterKit/iphoneos/TwitterKit.framework.dSYM/Contents/Resources/DWARF/TwitterKit \
   TwitterKit/iphonesimulator/TwitterKit.framework.dSYM/Contents/Resources/DWARF/TwitterKit
 
-lipo -archs iOS/TwitterKit.framework.dSYM/Contents/Resources/DWARF/TwitterKit
-
-## Zip them into TwitterKit.zip
-rm TwitterKit.zip
-zip -r TwitterKit.zip iOS/*
-rm -rf iOS
+lipo -archs build/TwitterKit.framework.dSYM/Contents/Resources/DWARF/TwitterKit
